e6ec9496a62e66fc10624f59e71802764ce9d3f6
Artem Rudiakov <sanctus9@gmail.com>
2024-10-03 12:38:22 +0400

    transition to net8

---
+++ src/MjmlTemplateApi/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MjmlEmailTemplate.Shared.Models.Profile;
+using MjmlTemplateApi.ViewEngineServices;
+
+namespace MjmlTemplateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : Controller
+    {
+        private readonly IViewProcessor _viewProcessor;
+
+        public ProfileController(IViewProcessor viewProcessor)
+        {
+            _viewProcessor = viewProcessor;
+        }
+
+        [HttpPost("profileRegistrationSuccess")]
+        public async Task<ActionResult> ProfileRegistrationSuccess([FromBody] ProfileRegistrationSuccessModel model, [FromHeader] bool isDebug = false)
+        {
+            if (isDebug)
+                return await _viewProcessor.DebugView(model, ControllerContext);
+
+            var body = await _viewProcessor.RenderHtml(model, ControllerContext);
+
+            return Created("Ok", body);
+        }
+    }
+}
---
+++ src/MjmlTemplateApi/Models/DebugHtmlViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MjmlTemplateApi.Models;
+
+public class DebugHtmlViewModel
+{
+    public string HtmlRaw { get; set; }
+}
